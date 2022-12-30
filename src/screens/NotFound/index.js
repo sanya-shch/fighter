4908fdaccd05ec583b4b725f4529892a57f7ab9b
sanya-shch <shchipkov.sanya@ukr.net>
2022-12-31 00:14:42 +0200

    added NotFound page

--- src/screens/NotFound/index.js
+++ src/screens/NotFound/index.js
@@ -2,8 +2,20 @@ import React from "react";
 
 import "./style.scss";
 
-const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
-};
+const NotFoundPage = () => (
+  <div className="not_found_page">
+    <section className="error-container">
+      <span className="four">
+        <span className="screen-reader-text">4</span>
+      </span>
+      <span className="zero">
+        <span className="screen-reader-text">0</span>
+      </span>
+      <span className="four">
+        <span className="screen-reader-text">4</span>
+      </span>
+    </section>
+  </div>
+);
 
 export default NotFoundPage;
